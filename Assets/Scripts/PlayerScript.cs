b2019af69f7748d41c24ac75e6e71d5722a867b4
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2018-02-11 12:52:58 +0900

    ・初コミット

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	[SerializeField] float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position += speed * transform.TransformDirection (Vector3.up);
+
+		Move ();
+
+
+	}
+
+	//PLayerの移動
+	void Move()
+	{
+		//y軸正方向に進む
+		this.gameObject.transform.position += speed * transform.TransformDirection (Vector3.up);
+
+		//右に進む
+		if(Input.GetKeyDown("right") == true){
+			//			transform.Rotate(new Vector3(0,1,0));
+			this.transform.rotation = Quaternion.Euler(0, 0, 270);
+		}
+
+		//左に進む
+		if(Input.GetKeyDown("left") == true){
+			this.transform.rotation = Quaternion.Euler(0, 0, 90);
+		}
+
+		//上に進む
+		if(Input.GetKeyDown("up") == true){
+			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+
+		//下に進む
+		if(Input.GetKeyDown("down") == true){
+			this.transform.rotation = Quaternion.Euler(0, 0, 180);
+		}
+
+
+
+
+	}
+
+
+}
